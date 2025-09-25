cf663bf4b2a96cf9c98f12db07344b8931f47fbc
Sebastian Ronceria <jronceria@ucundinamarca.edu.co>
2025-09-24 21:06:43 -0500

    cambios

--- MCD.py
+++ MCD.py
@@ -2,3 +2,10 @@ def mcd(a, b):
     # Caso base
     if b == 0:
         return a
+    # Caso recursivo
+    else:
+        return mcd(b, a % b)
+
+print(mcd(48, 18))  
+print(mcd(56, 98))  
+print(mcd(101, 103)) 
