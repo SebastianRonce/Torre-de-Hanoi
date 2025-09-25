63eb70d51e1b95f254f872afdea95a4a2f214156
SebastianRonce <jronceria@ucundinamarca.edu.co>
2025-09-24 20:07:22 -0500

    Update MCD.py

--- MCD/MCD.py
+++ MCD/MCD.py
@@ -5,8 +5,7 @@ def mcd(a, b):
     # Caso recursivo
     else:
         return mcd(b, a % b)
-
-# Ejemplo de uso
-print(mcd(48, 18))  # Resultado esperado: 6
-print(mcd(56, 98))  # Resultado esperado: 14
-print(mcd(101, 103))  # Resultado esperado: 1
+        
+print(mcd(48, 18))  
+print(mcd(56, 98))  
+print(mcd(101, 103))  
